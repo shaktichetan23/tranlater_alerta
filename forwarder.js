52e737f5bdb2919c86e932a8df2db6d33bdbf254
shaktichetan23 <hosdurga.s@husky.neu.edu>
2020-11-27 20:19:19 -0500

    Changes to forwarder

--- forwarder.js
+++ forwarder.js
@@ -4,11 +4,27 @@ const router = express.Router();
 const axios = require("axios");
 router.post("/", (req, res) => {
   let data = req.body;
+
+  let event = data.event.eventName;
+  let severity = data.event.severity;
+  let resource = data.kubernetes.host;
+  let text = data.event.message;
+  let service = data.kubernetes.namespace_name;
+
+  let payload = {
+    resource: resource,
+    event: event,
+    severity: severity.toLowerCase(),
+    environment: "Development",
+    service: [service],
+  };
+
+  console.log(payload);
   //   console.log(data.new_server);
   // data.resource = data.new_server;
   // delete data.new_server;
-  console.log("here");
-  console.log(data.log);
+  // console.log("here");
+  // console.log(data.log);
   const header = {
     "Content-Type": "application/json",
   };
@@ -17,13 +33,13 @@ router.post("/", (req, res) => {
     url: "http://localhost:8080/alert",
     method: "post",
     header: header,
-    data: req.body,
+    data: payload,
   })
     .then((result) => {
-      // console.log(result);
+      console.log(result);
     })
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
     });
 });
 module.exports = router;
---
+++ model.js
@@ -0,0 +1,3 @@
+processdata =(data)=>{
+    data.
+}
